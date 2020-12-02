8ac3faf902a5aae355af17761e71ba87df5c2caa
anup <anuppanta5555@gmail.com>
2020-12-02 08:25:34 +0545

    core http server example added

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello world');
+// //use of view engine
+
+// const webRoutes = require('./routes/web.js');
+// app.set('view engine', 'ejs');
+
+
+// app.use('/', webRoutes);
+
+const http = require('http');
+let port = 8000;
+
+//create new server
+let server = http.createServer((req, res) =>{
+	//handle the response
+
+	if(req.url == '/'){
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.write('<html><head> </head><body> <h1>Hello IIMS </h1> <p> This is our first pargraph.</p></body></html>');
+		res.end();
+	}else if(req.url === '/profile'){
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.write('<html><head> </head><body> <h1>Hello guys ... </h1> <p> This is our profile page.</p></body></html>');
+		res.end();
+
+	}
+	else{
+		res.writeHead(404, {'content-type' : 'text/html'});
+		res.write('<html><head> </head><body> <h1> 404 Page not found </h1></body></html>');
+		
+		res.end();
+	}
+})
+
+//listen server to port
+server.listen(port);
+console.log('server is running on port: ', port);
